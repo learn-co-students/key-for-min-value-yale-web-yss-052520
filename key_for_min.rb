666742e73c79a7ea7c600a4f4673e1120be0217f
Chad Palmer <chad.palmer@yale.edu>
2020-05-16 17:48:53 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+   result = name_hash.reduce() { |m, k| m.last > k.last ? k : m }
+   
+   if result
+     result.first
+   else
+     nil
+   end
 end
+
